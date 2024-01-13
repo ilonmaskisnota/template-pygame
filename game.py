1ff2b6571e61a6d718daaa71eabb8262cb2c3922
ilonmaskisnota <153349058+ilonmaskisnota@users.noreply.github.com>
2024-01-13 16:29:06 +1000

    Add files via upload

--- game.py
+++ game.py
@@ -20,21 +20,41 @@ running = True
 red = 0 
 green = 0 
 blue = 0 
+isRedMax = False
+
+
+y = 0
+
+
+
 while running:
  # Держим цикл на правильной скорости 
     clock.tick(FPS)
     screen.fill((red, green, blue))
-    green = green + 1
-    red = red + 1
-    if green == 255:
-        green = 0
-    if red == 255:
-        red = 0
+    # green = green + 1
+
+    if isRedMax == True:
+        green = green + 1
+    else:
+         red = red + 1
+         if red == 255:
+          red = 0
+          isRedMax = True
+    print(red)
+    
+
+
+    pygame.draw.rect(screen,(100,50,200),[0,y,100,200])  
+    y = y +0.1
+
         #Ввод процесса (события)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False 
     pygame.display.flip()
+    
+    
+  
 
 
 pygame.quit()
